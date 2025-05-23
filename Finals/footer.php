bbbfc80952594be5b87b826c3993c902cbb73f88
RJ Bernales <rainieljevson0@gmail.com>
2025-05-23 20:47:13 +0800

    adding header and footer which is separated to the pages

---
+++ Finals/footer.php
@@ -0,0 +1,33 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="index.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> 09XXXXXXXX </p>
+         <p> <i class="fas fa-phone"></i> 09XXXXXXXX </p>
+         <p> <i class="fas fa-envelope"></i> rj@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Manolo Fortich Bukidnon</p>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="facebook.com"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="twitter.com"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="instagram.com"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="linkedin.com"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; Copyright  @ <?php echo date('Y'); ?>  <span>BooKeeper</span> </p>
+
+</section>
